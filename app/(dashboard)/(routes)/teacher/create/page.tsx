b12f5b45bdfe20b48de157547c8fe5f647249a63
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-12-27 03:55:30 +0100

    Front-End: Added Form using zod

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,7 +1,82 @@
 "use client";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import {
+  Form,
+  FormControl,
+  FormLabel,
+  FormDescription,
+  FormField,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+const formSchema = z.object({
+  title: z.string().min(3, { message: "Title is required." }),
+});
+const CreateCourse = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  const { isSubmitting, isValid } = form.formState;
+  // To submit the form, we need to use the `handleSubmit` method from the `form` object.
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
-const createCourse = () => {
-  return <div>createCourse</div>;
+  return (
+    <div className="max-w-5xl mx-auto flex flex-col md:items-center md:justify-center h-full p-6">
+      <h1 className="text-2xl">Give your course a name</h1>
+      <p className="text-sm text-slate-500">
+        This is the first step to creating your course. You can always change
+        the name later.
+      </p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="title">Course title</FormLabel>
+                <FormControl>
+                  <Input
+                    id="title"
+                    placeholder="Introduction to React"
+                    {...field}
+                    disabled={isSubmitting}
+                  />
+                </FormControl>
+                <FormDescription>
+                  The name of your course as it will appear on your course page
+                  and on the course card.
+                </FormDescription>
+                <FormMessage {...field} />
+              </FormItem>
+            )}
+          />
+          <div className="flex items-center gap-x-2">
+            <Link href="/">
+              <Button variant="destructive" type="button">
+                Cancel
+              </Button>
+            </Link>
+            <Button type="submit" disabled={!isValid || isSubmitting}>
+              Continue
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
-export default createCourse;
+export default CreateCourse;
