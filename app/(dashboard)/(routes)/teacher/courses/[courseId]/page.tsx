30bc9935b62018b3a1bf104f1d4fe74b6eeeafc8
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-12-29 21:59:51 +0100

    FrontEnd: Refactor CourseIdPage component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,6 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { boolean } from "zod";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -29,7 +28,27 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
   const completedFields = requiredFields.filter(Boolean).length; // This will return the number of fields that are not null or undefined
 
-  return <div>CourseIdPage:{params.courseId}</div>;
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6 ">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Overview</h1>
+          <span className="text-sm text-slate-500">
+            Complete All fields: {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-xl font-medium">Customize the course</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
