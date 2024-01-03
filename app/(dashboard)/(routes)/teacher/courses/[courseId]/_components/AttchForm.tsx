d6f65634c1c7cb4e8c04d5891cc3289dc0ebadff
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-01-03 13:04:00 +0100

    Front- End: Update AttchForm component to display attachments and allow deletion

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttchForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttchForm.tsx
@@ -3,13 +3,13 @@ import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
 
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 import { Attachment, Course } from "@prisma/client";
-import Image from "next/image";
+
 import FileUpload from "@/components/fileUpload";
 
 interface AttachmentFormProps {
@@ -23,6 +23,7 @@ const formSchema = z.object({
 const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) => {
   const router = useRouter();
   const [isEditing, setIsEditing] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
   //
   const toggleEdit = () => {
     setIsEditing((current) => !current);
@@ -66,6 +67,36 @@ const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) => {
             <p className="text-sm mt-2 text-slate-500 italic ">
               No attachments yet
             </p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {
+                // @ts-ignore
+                initialData.attachments.map((attachment) => (
+                  <div
+                    className="flex items-center p-3 rounded-md bg-slate-200 space-x-2 border border-slate-300 text-sky-700"
+                    key={attachment.id}
+                  >
+                    <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                    <p className="text-sm line-clamp-1">{attachment.name}</p>
+                    {deletingId === attachment.id && (
+                      <div>
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      </div>
+                    )}
+                    {deletingId !== attachment.id && (
+                      <button
+                        className="ml-auto hover:opacity-75 transition"
+                        title="delete"
+                        type="button"
+                      >
+                        <X className="h-4 w-4" />
+                      </button>
+                    )}
+                  </div>
+                ))
+              }
+            </div>
           )}
         </>
       )}
