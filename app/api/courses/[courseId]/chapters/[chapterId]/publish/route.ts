66628011bd83fe6f14ca3b63e6b5e77a12fd47e4
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-02 23:04:46 +0100

    Back-End : Added Publish api routes

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/publish/route.ts
@@ -0,0 +1,53 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function PATCH(
+    req: Request,
+    {
+        params
+    }: {
+        params: {
+            courseId: string,
+            chapterId: string
+        }
+    }
+) {
+    try {
+        const { userId } = auth()
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+        const ownCourse = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId
+            }
+        })
+        if (!ownCourse) {
+            return new NextResponse("Forbidden", { status: 403 })
+        }
+        const chapter = await db.chapter.findUnique({
+            where: {
+                id: params.chapterId,
+                courseId: params.courseId
+            }
+        })
+        if (!chapter) {
+            return new NextResponse("Not Found", { status: 404 })
+        }
+        const updatedChapter = await db.chapter.update({
+            where: {
+                id: params.chapterId
+            },
+            data: {
+                isPublished: true
+            }
+        })
+        return NextResponse.json(updatedChapter)
+
+    } catch (error) {
+        console.log("[Chapters] [PATCH] error", error)
+    }
+
+}
