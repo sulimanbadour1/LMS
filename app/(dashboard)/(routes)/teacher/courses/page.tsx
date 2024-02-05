d2f060c689588b0096e30f582ce7cdbea74a780e
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-05 19:26:01 +0100

    Front-End: Added Data table from shadcn-ui

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
 import React from "react";
+import { DataTable } from "./_components/dataTable";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+async function getData(): Promise<any[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const CoursesPage = async () => {
+  const data = await getData();
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>Add Course</Button>
-      </Link>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
