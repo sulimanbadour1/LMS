70246df46f242edea5035313039eb81da90a4182
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-07 16:20:57 +0100

    Front-End: Add search input to NavbarRoutes component

---
+++ components/searchInput.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="w-full border border-slate-200 rounded-lg p-2"
+      />
+    </div>
+  );
+};
