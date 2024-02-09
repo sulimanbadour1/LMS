dab8f4e38295cd720dfb5491a9d971bf971b5a17
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-09 13:55:10 +0100

    Front-End: Refactor CoursesList component to use a grid layout and display a message when no courses are found

--- components/CoursesList.tsx
+++ components/CoursesList.tsx
@@ -12,11 +12,16 @@ interface CoursesListProps {
 export const CoursesList = ({ items }: CoursesListProps) => {
   return (
     <div>
-      {items.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <CourseCard key={item.id} course={item} />
+        ))}
+      </div>
+      {items.length === 0 && (
+        <div className="text-center text-xl text-muted-foreground mt-10">
+          No courses found
         </div>
-      ))}
+      )}
     </div>
   );
 };
