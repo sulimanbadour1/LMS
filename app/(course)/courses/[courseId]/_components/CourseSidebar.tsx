30a65bf63636099d8c8db25a3715c6b91867c745
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-10 11:28:56 +0100

    Front-End: Add CourseSidebarItem component to CourseSidebar

--- app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 import { redirect } from "next/navigation";
+import CourseSidebarItem from "./CourseSidebarItem";
 
 interface CourseSidebarProps {
   course: Course & {
@@ -30,6 +31,18 @@ const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
         {/* Check purchase and add progress*/}
+      </div>
+      <div className="flex flex-col w-full">
+        {course.chapters.map((chapter) => (
+          <CourseSidebarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={chapter.userProgress?.[0]?.isCompleted || false}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
+        ))}
       </div>
     </div>
   );
