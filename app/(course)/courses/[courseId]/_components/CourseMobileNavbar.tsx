ecc2f750896423b7d549175090d650cead53037e
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-13 00:40:05 +0100

    Front-End: Add Mobile nav bar

--- app/(course)/courses/[courseId]/_components/CourseMobileNavbar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseMobileNavbar.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { Menu } from "lucide-react";
+import { Chapter, UserProgress, Course } from "@prisma/client";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import CourseSidebar from "./CourseSidebar";
 
-export const CourseMobileNavbar = () => {
-  return <div>CourseMobileNavbar</div>;
+interface CourseMobileNavbarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+
+  progressCount: number;
+}
+
+export const CourseMobileNavbar = ({
+  course,
+  progressCount,
+}: CourseMobileNavbarProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu className="w-8 h-8" />
+      </SheetTrigger>
+      <SheetContent side={"left"} className="p-0 bg-white w-72">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </SheetContent>
+    </Sheet>
+  );
 };
