5fc75558421d6987eeb24d38193229321661dd73
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-14 13:22:05 +0100

    Back-End: Added get chapter func

--- app/(course)/courses/[courseId]/_components/CourseSidebarItem.tsx
+++ app/(course)/courses/[courseId]/_components/CourseSidebarItem.tsx
@@ -32,7 +32,7 @@ const CourseSidebarItem = ({
       className={cn(
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 ",
         isActive &&
-          "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-200/20",
+          "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-500",
         isCompleted && "text-emerald-700 hover:text-emerald-700",
         isCompleted && isActive && "bg-emerald-200/20"
       )}
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChapterIdPage = () => {
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+}) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const;
+
   return <div>ChapterIdPage</div>;
 };
 
