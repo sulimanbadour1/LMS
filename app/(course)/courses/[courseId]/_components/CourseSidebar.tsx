17eacccfbea5b856aa3ffb6ca6d68b8cf618a7c1
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2024-02-14 12:44:11 +0100

    Front-End: Refactor CourseSidebar component

--- app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
@@ -28,7 +28,7 @@ const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
 
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
-      <div className="p-8 flex flex-col border-b">
+      <div className="p-9 flex flex-col ">
         <h1 className="font-semibold">{course.title}</h1>
         {/* Check purchase and add progress*/}
       </div>
